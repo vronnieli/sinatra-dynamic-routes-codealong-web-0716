527539e5d96c43334d0682d73d492ffe552af292
vronnieli <vronnieli@gmail.com>
2016-08-11 19:18:22 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,5 @@
 require_relative 'config/environment'
+require 'pry'
 
 class App < Sinatra::Base
 
@@ -11,6 +12,15 @@ class App < Sinatra::Base
   get "/hello/:name" do
     @user_name = params[:name]
     "Hello #{@user_name}!"
+  end
+
+  get '/goodbye/:name' do
+    @name = params[:name]
+    "Goodbye #{@name}!"
+  end
+
+  get '/multiply/:num1/:num2' do
+    "#{params[:num1].to_i * params[:num2].to_i}"
   end
 
 
